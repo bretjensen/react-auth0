49ff11d1bc22b71c62cc6d344850b2cfdea5c639
Brett Jensen <bretjensen@gmail.com>
2019-06-28 07:12:35 +0100

    Quotes and prettier config file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import Home from "./Home";
-import Profile from "./Profile";
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
+import Profile from './Profile'
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" exact component={Profile} />
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Route path="/" exact component={Home} />
+                <Route path="/profile" exact component={Profile} />
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
